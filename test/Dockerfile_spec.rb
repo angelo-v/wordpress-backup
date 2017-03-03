2ca882c3a5ce3992eb5178ce7b00772c7ad8ad6c
Angelo Veltens <angelo.veltens@codecentric.de>
2017-03-03 20:01:10 +0100

    added further tests

--- test/Dockerfile_spec.rb
+++ test/Dockerfile_spec.rb
@@ -4,11 +4,24 @@ require "docker"
 
 describe "Dockerfile" do
   before(:all) do
-    image = Docker::Image.build_from_dir('../src')
+    @image = Docker::Image.build_from_dir('../src')
+
+    @container = Docker::Container.create(
+      'Image' => @image.id,
+      'Name' => '/wordpress-backup-test',
+      'Env' => [
+        'MYSQL_ENV_MYSQL_USER=test_user',
+        'MYSQL_ENV_MYSQL_DATABASE=test_db',
+        'MYSQL_ENV_MYSQL_PASSWORD=test_password',
+        'CLEANUP_OLDER_THAN=300'
+      ]
+    )
+    @container.start
+    set :docker_container, @container.id
 
     set :os, family: :debian
     set :backend, :docker
-    set :docker_image, image.id
+    set :docker_container, @container.id
   end
 
   it "installs required packages" do
@@ -20,6 +33,27 @@ describe "Dockerfile" do
   it "contains the backup and restore commands" do
     expect(file('/bin/backup')).to be_file
     expect(file('/bin/restore')).to be_file
+  end
+
+  describe command('env') do
+   its(:stdout) { should match /MYSQL_ENV_MYSQL_USER=test_user/ }
+   its(:stdout) { should match /MYSQL_ENV_MYSQL_DATABASE=test_db/ }
+   its(:stdout) { should match /MYSQL_ENV_MYSQL_PASSWORD=test_password/ }
+   its(:stdout) { should match /CLEANUP_OLDER_THAN=300/ }
+   its(:stdout) { should match /BACKUP_TIME=0 3 \* \* \*/ }
+  end
+
+  it "contains cron file" do
+    expect(file('/backup-cron')).to be_file
+  end
+
+  describe command('crontab -l') do
+   its(:stdout) { should eq %(MYSQL_ENV_MYSQL_USER=test_user
+MYSQL_ENV_MYSQL_DATABASE=test_db
+MYSQL_ENV_MYSQL_PASSWORD=test_password
+CLEANUP_OLDER_THAN=300
+0 3 * * * backup > /backup.log
+) }
   end
 
 end
