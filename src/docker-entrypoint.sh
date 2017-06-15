b3e0e6a59a76def163278be1fa61bd031dcfa99c
Matías Menich <matimenich@gmail.com>
2017-06-15 11:45:41 -0400

    Adding MYSQL_HOST as environment variable

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -5,6 +5,7 @@ then
   echo "Creating cron entry to start backup at: $BACKUP_TIME"
   # Note: Must use tabs with indented 'here' scripts.
   cat <<-EOF >> backup-cron
+MYSQL_ENV_MYSQL_HOST=$MYSQL_ENV_MYSQL_HOST
 MYSQL_ENV_MYSQL_USER=$MYSQL_ENV_MYSQL_USER
 MYSQL_ENV_MYSQL_DATABASE=$MYSQL_ENV_MYSQL_DATABASE
 MYSQL_ENV_MYSQL_PASSWORD=$MYSQL_ENV_MYSQL_PASSWORD
