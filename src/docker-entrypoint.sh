8be29cc91c921001a849cc23c19cfc6b40e3caf7
mrcbns <moracabanas@live.com>
2021-01-06 17:30:17 +0100

    Update docker-entrypoint.sh
    Stolen from mysql Dockerfile, this change allows to read secrets from files

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		mysql_error "Both $var and $fileVar are set (but are exclusive)"
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
 if ! [ -f backup-cron ]
 then
   echo "Creating cron entry to start backup at: $BACKUP_TIME"
   # Note: Must use tabs with indented 'here' scripts.
   cat <<-EOF >> backup-cron
-MYSQL_ENV_MYSQL_HOST=$MYSQL_ENV_MYSQL_HOST
-MYSQL_ENV_MYSQL_USER=$MYSQL_ENV_MYSQL_USER
-MYSQL_ENV_MYSQL_DATABASE=$MYSQL_ENV_MYSQL_DATABASE
-MYSQL_ENV_MYSQL_PASSWORD=$MYSQL_ENV_MYSQL_PASSWORD
+file_env 'MYSQL_ENV_MYSQL_HOST'
+file_env 'MYSQL_ENV_MYSQL_USER'
+file_env 'MYSQL_ENV_MYSQL_DATABASE'
+file_env 'MYSQL_ENV_MYSQL_PASSWORD'
 EOF
 
   if [[ $CLEANUP_OLDER_THAN ]]
